359851c9b395c498410a18c10a9f31d1d0b8a8f0
yizhou136 <yizhou136@gmail.com>
2017-06-05 11:25:14 +0800

    update config

--- zystudio-registry-center/src/main/docker/create_services.sh
+++ zystudio-registry-center/src/main/docker/create_services.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 
+#HOSTNAME_TEMPLATE="${ZONE}{{.Task.Slot}}.{{.Service.Name}}.zystudio.com"
 echo "create registry conter services for zystudio"
 
 NETWORK=zystudio_common
 IMAGE="my.docker.registry.com:5000/zystudio/registry_center:0.0.1"
 PROJECT="regcen"
-ZONE="bj_yw_1"
-HOSTNAME_TEMPLATE="${ZONE}{{.Task.Slot}}.{{.Service.Name}}.zystudio.com"
-NAME=${PROJECT}-${ZONE}
-echo $HOSTNAME_TEMPLATE
 
-docker-machine ssh manager1 docker  service  create  --replicas 1 --network ${NETWORK} --label service_name=${NAME} --hostname=${HOSTNAME_TEMPLATE}  --env zone=${ZONE} --env slot="{{.Task.Slot}}" --constraint engine.labels.location==${ZONE} --constraint engine.labels.type==common --name ${NAME}  ${IMAGE}
 
-ZONE="bj_dx_1"
-HOSTNAME_TEMPLATE="${ZONE}{{.Task.Slot}}.{{.Service.Name}}.zystudio.com"
-NAME=${PROJECT}-${ZONE}
-docker-machine ssh manager1 docker  service  create  --replicas 1 --network ${NETWORK} --label service_name=${NAME} --hostname=${HOSTNAME_TEMPLATE}  --env zone=${ZONE} --env slot="{{.Task.Slot}}" --constraint engine.labels.location==${ZONE}  --name ${NAME}  ${IMAGE}
+ZONE="bjyw1"
+NAME="${ZONE}.${PROJECT}.zystudio.com"
+docker-machine ssh manager1 docker  service  create  --replicas 1 --network ${NETWORK} --label service_name=${NAME} --hostname=${NAME}  --env zone=${ZONE} --env slot="{{.Task.Slot}}" --constraint engine.labels.location==${ZONE} --constraint engine.labels.type==common --name ${NAME}  ${IMAGE}
+
+ZONE="bjdx1"
+NAME="${ZONE}.${PROJECT}.zystudio.com"
+docker-machine ssh manager1 docker  service  create  --replicas 1 --network ${NETWORK} --label service_name=${NAME} --hostname=${NAME}  --env zone=${ZONE} --env slot="{{.Task.Slot}}" --constraint engine.labels.location==${ZONE} --constraint engine.labels.type==common --name ${NAME}  ${IMAGE}
