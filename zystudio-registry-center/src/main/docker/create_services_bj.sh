8d704a06706ca0f2876f36c0f018cbd5f5ac5519
yizhou136 <yizhou136@gmail.com>
2017-07-02 00:22:47 +0800

    update   docker

--- zystudio-registry-center/src/main/docker/create_services_bj.sh
+++ zystudio-registry-center/src/main/docker/create_services_bj.sh
@@ -17,21 +17,23 @@ LOCATION=${location:-"bj"}
 #SET_ENV="--env zone=${ZONE} --env slot=\"{{.Task.Slot}}\"
 #--env registry.peers=\"http://regcen-bjyw1:1100/eureka\"  --env registry.hostname=${NAME}"
 
-SERVICE_NAME="${ORG_SERVICE_NAME}-${LOCATION}dx01"
+PROFILE="${LOCATION}dx01"
+SERVICE_NAME="${ORG_SERVICE_NAME}-${PROFILE}"
 echo "create ${SERVICE_NAME} service for ${IMG_PREF}"
 #HOST_NAME="${SERVICE_NAME}-${LOCATION}{{.Task.Slot}}"
 SET_COMMS="--publish 1100:1100 --replicas 1 --network ${NETWORK} --label service.name=${SERVICE_NAME} --hostname=${HOST_NAME} --name ${SERVICE_NAME}"
-SET_ENV="--env service=${SERVICE_NAME} --env location=${LOCATION} --env slot={{.Task.Slot}} --env profile=${LOCATION}"
+SET_ENV="--env service=${SERVICE_NAME} --env location=${LOCATION} --env slot={{.Task.Slot}} --env profile=${PROFILE}"
 SET_CONSTRAINT="--constraint engine.labels.location==${LOCATION} --constraint engine.labels.service.type==common"
 echo "docker service create ${SET_COMMS} ${SET_ENV} ${SET_CONSTRAINT} ${IMAGE_NAME}"
 docker network  create  -d  overlay  ${NETWORK}
 docker  service  create ${SET_COMMS} ${SET_ENV} ${SET_CONSTRAINT}  ${IMAGE_NAME}
 
 
-SERVICE_NAME="${ORG_SERVICE_NAME}-${LOCATION}yw01"
+PROFILE="${LOCATION}yw01"
+SERVICE_NAME="${ORG_SERVICE_NAME}-${PROFILE}"
 echo "create ${SERVICE_NAME} service for ${IMG_PREF}"
 SET_COMMS="--publish 1101:1101 --replicas 1 --network ${NETWORK} --label service.name=${SERVICE_NAME} --hostname=${SERVICE_NAME} --name ${SERVICE_NAME}"
-SET_ENV="--env service=${SERVICE_NAME} --env location=${LOCATION} --env slot={{.Task.Slot}} --env profile=${LOCATION}"
+SET_ENV="--env service=${SERVICE_NAME} --env location=${LOCATION} --env slot={{.Task.Slot}} --env profile=${PROFILE}"
 SET_CONSTRAINT="--constraint engine.labels.location==${LOCATION} --constraint engine.labels.service.type==common"
 echo "docker service create ${SET_COMMS} ${SET_ENV} ${SET_CONSTRAINT} ${IMAGE_NAME}"
 docker network  create  -d  overlay  ${NETWORK}
