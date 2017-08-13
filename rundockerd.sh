976e069dff663ae900a1c1cb97bca3e29999d6df
yizhou136 <yizhou136@gmail.com>
2017-08-13 20:56:46 +0800

    update  zystudio-api-gateway

--- rundockerd.sh
+++ rundockerd.sh
@@ -6,4 +6,5 @@ SRV_TYPE=${2:-"common"}
 dockerd -D -H=unix:///var/run/docker.sock -H tcp://0.0.0.0:2375 --label location=${LOCATION} --label service.type=${SRV_TYPE}
 
 
-#docker  run    -d --name  jenkins   -p 50000:50000  -p 8080:8080  jenkins
+#docker  run   -d --name  jenkins       -p 50000:50000  -p 8080:8080  jenkins
+#docker  run   -d --name  jenkins_work   -p 50000:50000 -p 8080:8080  -v /work/jenkins_run/_data:/var/jenkins_home  jenkins
