11fb91419a152318afee1e90ade64963d559dd39
yizhou136 <yizhou136@gmail.com>
2017-09-02 23:56:31 +0800

    update  user service

--- zystudio-micro-services/user-service/src/main/docker/create_services.sh
+++ zystudio-micro-services/user-service/src/main/docker/create_services.sh
@@ -12,6 +12,8 @@ IMAGE_BASE_NAME=${IMG_PREF}/${ORG_SERVICE_NAME}
 DEP_IMAGE_NAME=${IMAGE_BASE_NAME}-dep:${SERVICE_VER}
 IMAGE_NAME=${IMAGE_BASE_NAME}:${SERVICE_VER}
 
+
+HOST_IP=`ifconfig eth0 | grep "inet addr:" | awk '{print $2}' | cut -c 6-`
 NETWORK=zystudio_common
 LOCATION=${location:-"bj"}
 #HOSTNAME_TEMPLATE="${ZONE}{{.Task.Slot}}.{{.Service.Name}}.zystudio.com"
@@ -23,7 +25,7 @@ PROFILE="dev"
 SERVICE_NAME="${ORG_SERVICE_NAME}-${PROFILE}"
 echo "create ${SERVICE_NAME} service for ${IMG_PREF}"
 
-SET_COMMS="--publish 4000:4000 --replicas 1 --network ${NETWORK} --label service.name=${SERVICE_NAME} --hostname=${SERVICE_NAME} --name ${SERVICE_NAME}"
+SET_COMMS="--publish 4000:4000 --replicas 1 --network ${NETWORK} --label service.name=${SERVICE_NAME} --hostname=${SERVICE_NAME} --name ${SERVICE_NAME} --host datasource-host:${HOST_IP}"
 SET_ENV="--env service=${SERVICE_NAME} --env location=${LOCATION} --env slot={{.Task.Slot}} --env profile=${PROFILE}"
 SET_CONSTRAINT="--constraint engine.labels.location==${LOCATION} --constraint engine.labels.service.type==common"
 
